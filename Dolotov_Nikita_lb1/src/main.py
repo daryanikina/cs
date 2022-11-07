68055649c086c4c165a72425c9640411b543d6a6
do1otov <dolotok2@gmail.com>
2022-11-08 00:25:14 +0300

    Dolotov_Nikita_lb1

--- Dolotov_Nikita_lb1/src/main.py
+++ Dolotov_Nikita_lb1/src/main.py
@@ -3,19 +3,18 @@ import numpy as np
 def check_crossroad(robot, point1, point2, point3, point4):
     if (point1[0] <= robot[0] <= point3[0]) and (point1[1] <= robot[1] <= point3[1]):
         return True
-    else:
-        return False
+    return False
 
 def check_collision(coefficients):
     fl_matrix = np.delete(coefficients, 2, 1)
     out = []
     for i in range(len(fl_matrix)):
-        for j in range(len(fl_matrix)):
-            if i == j: continue
+        for j in range(i + 1, len(fl_matrix)):
             matrix = np.concatenate(([fl_matrix[i]], [fl_matrix[j]]), axis=0)
             if np.linalg.det(matrix) != 0:
                 out.append((i, j))
-    return out
+                out.append((j, i))
+    return sorted(out)
 
 def check_path(points_list):
     path = 0
